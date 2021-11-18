48a2c27d76a27ff8219959f9088d8296ed78daa9
Ky Anh <40734986+itskyf@users.noreply.github.com>
2021-11-18 14:47:59 +0700

    Update ShapeFactory
    Delete assets

--- Shapes/ShapeFactory.cs
+++ Shapes/ShapeFactory.cs
@@ -1,30 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using Color = System.Windows.Media.Color;
 
 namespace SharpGLPaint.Shapes;
 
 public static class ShapeFactory {
-    private static readonly Dictionary<ShapeMode, Type> RegisteredShapes = new();
-
-    public static void Register<T>(ShapeMode key) {
-        if (RegisteredShapes.ContainsKey(key)) {
-            throw new ArgumentException($"Key {key} has existed");
-        }
-
-        var type = typeof(T);
-        if (type.IsAbstract || type.IsInterface) {
-            throw new ArgumentException("Cannot create instance of interface or abstract class");
-        }
-
-        RegisteredShapes.Add(key, type);
-    }
-
     public static Shape Create(ShapeMode key, Point startPoint, Point endPoint, Color color, float pointSize) {
-        var createdShape =
-            Activator.CreateInstance(RegisteredShapes[key], startPoint, endPoint, color, pointSize) ??
-            throw new ArgumentException($"Cannot create {key}");
-        return (Shape) createdShape;
+        Shape shape = key switch {
+            ShapeMode.Line => new Line(startPoint, endPoint, color, pointSize),
+            // ShapeMode.Circle => expr,
+            ShapeMode.Rectangle => new Rectangle(startPoint, endPoint, color, pointSize),
+            // ShapeMode.Ellipse => expr,
+            ShapeMode.Square => new Square(startPoint, endPoint, color, pointSize),
+            // ShapeMode.Pentagon => expr,
+            // ShapeMode.Hexagon => expr,
+            _ => throw new ArgumentOutOfRangeException(nameof(key), key, null)
+        };
+
+        return shape;
     }
 }
